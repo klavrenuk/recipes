98da96814fcb67a375fb61bdbbf3bff149143fa8
klavrenuk <klavrenuk@yandex.ru>
2023-05-29 03:34:02 +0500

    Updates

---
+++ _nuxt/default.66f48476.js
@@ -0,0 +1,1 @@
+import{H as t}from"./HeaderGeneral.56699727.js";import{k as o,e as s,i as n,u as r,h as e,y as l,o as c}from"./entry.87689ebb.js";const d={class:"layout_default"},u={class:"container mx-auto"},p=o({__name:"default",setup(_){return(a,i)=>(c(),s("div",d,[n(r(t)),e("main",null,[e("div",u,[l(a.$slots,"default")])])]))}});export{p as default};
